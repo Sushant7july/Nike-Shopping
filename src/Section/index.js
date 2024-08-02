c0daaa6f26ba891ce7c7ea701753500ba1ad7326
Sushant Kumar <124777555+Sushant7july@users.noreply.github.com>
2024-08-02 22:04:50 +0530

    Add files via upload

---
+++ src/Section/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Subscribe from './Subscribe';
+import SpecialOffer from './SpecialOffer';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import Services from './Services';
+
+export{
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    CustomerReviews,
+    Footer
+}
